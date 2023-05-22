094c09f2a96fdff4bb3fd7734ed441ca115caa58
PingKhoe <grace.m.khoe@gmail.com>
2023-05-22 20:56:09 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/ibda/uas_pads/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ibda.uas_pads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
