927781bf11b64b185f11a8dbef73f4f78c530204
PingKhoe <grace.m.khoe@gmail.com>
2023-05-28 18:10:36 +0700

    ikura

--- app/src/main/java/id/ac/ibda/uas_pads/CL_Customers.kt
+++ app/src/main/java/id/ac/ibda/uas_pads/CL_Customers.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import id.ac.ibda.uas_pads.databinding.FragmentCLCustomersBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,12 +22,26 @@ class CL_Customers : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentCLCustomersBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+        binding = FragmentCLCustomersBinding.inflate(layoutInflater)
+
+        binding.orderBtn.setOnClickListener {
+            findNavController().navigate(R.id.orders)
+        }
+
+        binding.customerBtn.setOnClickListener {
+            findNavController().navigate(R.id.customersList)
+        }
+
+        binding.inventoryBtn.setOnClickListener {
+            findNavController().navigate(R.id.inventory)
         }
     }
 
--- app/src/main/java/id/ac/ibda/uas_pads/Inventory.kt
+++ app/src/main/java/id/ac/ibda/uas_pads/Inventory.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import id.ac.ibda.uas_pads.databinding.FragmentInventoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,12 +22,22 @@ class Inventory : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentInventoryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+        binding = FragmentInventoryBinding.inflate(layoutInflater)
+
+        binding.orderBtn.setOnClickListener {
+            findNavController().navigate(R.id.orders)
+        }
+
+        binding.customerBtn.setOnClickListener {
+            findNavController().navigate(R.id.customersList)
         }
     }
 
